2d2f0f56a8a3ba3a3238f3342cdbb21cd1d7caf9
robincar000 <54599514+robincar000@users.noreply.github.com>
2020-01-10 14:42:34 -0600

    Add files via upload

--- main.py
+++ main.py
@@ -1,77 +1,29 @@
-import Controls as c
-import rospy
-#import Map as m
-import numpy as np
-#import Navigation as nav
-#import Lidar as Lidar
 import time
+import serial
+import keyboard
 
-from nav_msgs.msg import Odometry as odom
-from sensor_msgs.msg import LaserScan
+from obama import turnyBoi
 
-def run():
-    print("run method go plz?")
 
-def get_data_array(data, a=0, b=240):
-    PPD = 4.5  # points per degree
-    data = data[int(a * PPD):int(b * PPD)]
-    arr = np.array(data)
-    return arr
-def printData(data):
-    #print(data)
-    dataFront = get_data_array(data.ranges, 100, 140)
-    dataRight = get_data_array(data.ranges, 30-15, 30+15)
-    dataLeft = get_data_array(data.ranges, 240-30-15, 240-15)
-    distleft=dataLeft.mean()
-    distright=dataRight.mean()
+def main():
+    arduinoData = serial.Serial('com4', 9600)
 
-    distInFront = dataFront.mean()
+    # inputsAndTurning = turnyBoi(arduinoData)
+    # inputsAndTurning.setWPosition(40)
 
-    pturn = (distleft - distright) / (distleft + distright) / distInFront * 9
 
-    print(distInFront)
-    go=True
+    # to encode:
+    # str(chr(value)).encode()
 
-    x=1
-    speed=distInFront
+    while True:
+        if keyboard.is_pressed('a'):
+            arduinoData.write(str(chr(70)).encode())
+            time.sleep(.1)
+        if keyboard.is_pressed('d'):
+            #print(bytes(ord('a')))
+            arduinoData.write(str(chr(110)).encode())
+            time.sleep(.1)
 
-    if((distInFront>2 and distright>2) or (distInFront>2 and distleft>2)):#previously 3
-        turn=0
-    else:
-        turn=pturn
-
-    c.move(x,turn,speed)
 
 if __name__ == "__main__":
-    print ("go")
-    rospy.init_node('keyop')  # vesc/ackermann_cmd_mux/input/navigation ackermann_msgs/AckermannDriveStamped
-    #mpo=rospy.Subscriber('main', LaserScan,run , queue_size=2)
-    #pub = rospy.Subscriber('odom', odom, nav.print_odata, queue_size=2)
-    lidarsub = rospy.Subscriber('scan', LaserScan, printData, queue_size=2)
-lrender=0
-dt=0
-lastsave=rospy.get_time()
-imgnum=0
-lrtime=rospy.get_time()
-
-
-while not rospy.is_shutdown():
-    #m.update(Lidar.getOrient())
-    '''if(rospy.get_time()-lrtime>.5 and m.route!=None):
-        m.updateRoute()
-        lrtime=rospy.get_time()
-    if (rospy.get_time()>lrender+1.5):
-        st=time.time()
-        lrender=rospy.get_time()
-        m.render(dt)
-        dt=time.time()-st
-        #print ('render time : ',dt)
-    if (rospy.get_time() - lastsave > 8):
-        st1=time.time()
-        m.saveImg(imgnum)
-        lastsave = rospy.get_time()
-        imgnum = imgnum + 1
-        print ('img saved in ',(time.time()-st1),' sec')'''
-
-    pass
- 
+    main()
